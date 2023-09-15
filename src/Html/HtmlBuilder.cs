802bf753926c8734a573202500d6b94a83cde72f
Ricardo Coelho <tiorac@msn.com>
2023-09-15 18:46:52 -0300

    Update libs

---
+++ src/Html/HtmlBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackDigital.Report.Html
+{
+    public class HtmlBuilder : ReportBuilder
+    {
+        public override Task<byte[]> BuildAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
